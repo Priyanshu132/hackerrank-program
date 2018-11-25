c65a17b5e906a86ec330a1717433781057a5a31a
Priyanshu132 <45144924+Priyanshu132@users.noreply.github.com>
2018-11-25 19:27:12 +0530

    Add files via upload

---
+++ selection sorting.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[5],i,j,t,n=0;
+ clrscr();
+ for(i=0;i<=4;i++)
+ scanf("%d",&a[i]);
+ for(j=0;j<=4;j++)
+ {
+  for(i=0;i<=4;i++)
+   {
+    if(a[j]>a[i])
+     {
+      t=a[j];
+      a[j]=a[i];
+      a[i]=t;
+      n++;
+     }
+    }
+   }
+    for(i=0;i<=4;i++)
+    printf("%d ",a[i]);
+    getch();
+  }
